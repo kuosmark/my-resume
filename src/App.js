3252f70b3f70a27a1ba119820264cb80d94e149e
Markus Kuosmanen <markus.kuosmanen@helsinki.fi>
2019-01-28 06:21:14 +0200

    language switch

--- src/App.js
+++ src/App.js
@@ -1,82 +1,141 @@
 import React, { Component } from 'react'
 import myImage from './images/me2.jpg'
-import { Menu, Grid, Container, Image } from 'semantic-ui-react'
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+import htmlImage from './images/html5.png'
+import { Menu, Grid, Container, Image, Flag } from 'semantic-ui-react'
+import { BrowserRouter as Router, Route, Redirect, NavLink } from 'react-router-dom'
 
-class MainMenu extends Component {
-  state = { activeItem: 'whoami' }
+class App extends Component {
+  state = {
+    language: 'en',
+    activeItem: 'whoami'
+  }
 
   handleItemClick = (e, { name }) => {
     this.setState({ activeItem: name })
   }
 
+  changeLanguage = (e, { name }) => {
+    this.setState({ language: name })
+  }
+
   render() {
+    const style = { margin: 10 }
     const activeItem = this.state.activeItem
+    const language = this.state.language
 
     return (
-      <Menu stackable inverted /*pointing secondary*/>
-        <Menu.Item
-          name='whoami'
-          as={NavLink}
-          exact to='/'
-          active={activeItem === 'whoami'}
-          content='Who am I?'
-          onClick={this.handleItemClick}
-        />
+      < Container >
+        <Router>
+          <div>
+            <h1 style={style}>Markus Kuosmanen</h1>
 
-        <Menu.Item
-          name='skills'
-          as={NavLink}
-          exact to='/skills'
-          active={activeItem === 'skills'}
-          content='Skills'
-          onClick={this.handleItemClick}
-        />
+            <Menu stackable inverted /*pointing secondary*/>
+              <Menu.Item
+                name='whoami'
+                as={NavLink}
+                exact to={language === 'en' ? '/en' : '/fi'}
+                active={activeItem === 'whoami'}
+                content={language === 'en' ? 'Who am I?' : 'Kuka olen?'}
+                onClick={this.handleItemClick}
+              />
 
-        <Menu.Item
-          name='experience'
-          as={NavLink}
-          exact to='/experience'
-          active={activeItem === 'experience'}
-          content='Experience'
-          onClick={this.handleItemClick}
-        />
-      </Menu>
+              <Menu.Item
+                name='skills'
+                as={NavLink}
+                exact to={language === 'en' ? '/skills/en' : '/skills/fi'}
+                active={activeItem === 'skills'}
+                content={language === 'en' ? 'Skills' : 'Taidot'}
+                onClick={this.handleItemClick}
+              />
+
+              <Menu.Item
+                name='experience'
+                as={NavLink}
+                exact to={language === 'en' ? '/experience/en' : '/experience/fi'}
+                active={activeItem === 'experience'}
+                content={language === 'en' ? 'Experience' : 'Kokemus'}
+                onClick={this.handleItemClick}
+              />
+
+              <Menu.Item
+                name='interests'
+                as={NavLink}
+                exact to={language === 'en' ? '/interests/en' : '/interests/fi'}
+                active={activeItem === 'interests'}
+                content={language === 'en' ? 'Interests' : 'Kiinnostukset'}
+                onClick={this.handleItemClick}
+              />
+
+              <Menu.Menu position='right' >
+                {language === 'fi' ?
+                  <Menu.Item
+                    name='en'
+                    active={activeItem === 'toEnglish'}
+                    content={<Flag name='gb' />}
+                    onClick={this.changeLanguage}
+                  />
+                  :
+                  <Menu.Item
+                    name='fi'
+                    active={activeItem === 'toFinnish'}
+                    content={<Flag name='fi' />}
+                    onClick={this.changeLanguage}
+                  />}
+              </Menu.Menu>
+
+            </Menu >
+            <Route exact path='/en'
+              render={() => language === 'en' ? (<Me />) :
+                <Redirect to='/fi' />} />
+
+            <Route exact path='/fi'
+              render={() => language === 'fi' ? (<Mina />) :
+                <Redirect to='/en' />} />
+
+            <Route exact path='/skills/en'
+              render={() => language === 'en' ? (<Skills />) :
+                <Redirect to='/skills/fi' />} />
+
+            <Route exact path='/skills/fi'
+              render={() => language === 'fi' ? (<Taidot />) :
+                <Redirect to='/skills/en' />} />
+
+            <Route exact path='/experience/en'
+              render={() => language === 'en' ? (<Experience />) :
+                <Redirect to='/experience/fi' />} />
+
+            <Route exact path='/experience/fi'
+              render={() => language === 'fi' ? (<Kokemus />) :
+                <Redirect to='/experience/en' />} />
+
+            <Route exact path='/interests/en'
+              render={() => language === 'en' ? (<Interests />) :
+                <Redirect to='/interests/fi' />} />
+
+            <Route exact path='/interests/fi'
+              render={() => language === 'fi' ? (<Kiinnostukset />) :
+                <Redirect to='/interests/en' />} />
+          </div>
+        </Router>
+      </Container >
     )
   }
-}
-
-const App = () => {
-
-  const style = { margin: 10 }
-
-  return (
-    <Container>
-      <Router>
-        <div>
-          <h1 style={style}>Markus Kuosmanen</h1>
-          <MainMenu />
-          <div>
-            <Route exact path='/'
-              render={() => <Me />} />
-            <Route exact path="/skills"
-              render={() => <Skills />} />
-            <Route exact path='/experience'
-              render={() => <Experience />} />
-          </div>
-        </div>
-      </Router>
-    </Container>
-  )
 }
 
 const Me = () => {
   return (
     <div>
       <p>My name is Markus.</p>
+      <p>BIO</p>
+      <p>MY LIFE GOALS</p>
+      <p>Contact, Intro, Skillset, Job History, Other</p>
       <Image src={myImage} size={'small'} centered circular bordered />
     </div>
   )
+}
+
+const Mina = () => {
+  return <div>Olen suomalainen.</div>
 }
 
 const Skills = () => {
@@ -84,28 +143,32 @@ const Skills = () => {
     <div>
       <Grid /*celled*/ divided columns={3}>
         <Grid.Row>
-        <Grid.Column>
-          <div>1.1</div>
-        </Grid.Column>
-        <Grid.Column>
-          <div>1.2</div>
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row>
-        <Grid.Column>
-          <div>2.1</div>
-        </Grid.Column>
-        <Grid.Column>
-          <div>2.2</div>
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row>
-      </Grid.Row>
-      <Grid.Row>
-      </Grid.Row>
+          <Grid.Column>
+            <Image src={htmlImage} size={'small'} centered />
+          </Grid.Column>
+          <Grid.Column>
+            <div>I have skills in HTML.</div>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <div>2.1</div>
+          </Grid.Column>
+          <Grid.Column>
+            <div>2.2</div>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+        </Grid.Row>
+        <Grid.Row>
+        </Grid.Row>
       </Grid>
     </div >
   )
+}
+
+const Taidot = () => {
+  return <div>Taitoni ovat:</div>
 }
 
 const Experience = () => {
@@ -114,6 +177,18 @@ const Experience = () => {
       <p>This is my experience.</p>
     </div>
   )
+}
+
+const Kokemus = () => {
+  return <div>Tässä on kokemukseni.</div>
+}
+
+const Interests = () => {
+  return <div>My interests.</div>
+}
+
+const Kiinnostukset = () => {
+  return <div>Kiinnostukseni.</div>
 }
 
 export default App
