150ac00290abc9b8404e6cf6a12a84334af0dbd7
Markus Kuosmanen <markus.kuosmanen@helsinki.fi>
2019-02-04 23:02:20 +0200

    Heroku

---
+++ backend/Data.js
@@ -0,0 +1,44 @@
+const Data = {
+  en: {
+    bio: [
+      'I am a student and all that...',
+      'My Travel Tracker application',
+      '(React, Redux, Semantic.ui, Node.js, MongoDB)'
+    ],
+    skills: [
+      'In my studies I have used Java, Python, HTML, CSS Junit-testing and SQL-databases.',
+      'I got excited about web development on my Full Stack course. In my applications I use React, Redux, Node.js and MongoDB.',
+      'I have used Git version control in all my software projects. In my largest ones I use Heroku and Travis.'
+    ],
+    experience: [
+      'Software development project',
+      'We developed working software according to client requirements in a six-person Scrum team as part of a software engineering course. During four sprints we became familiar with client meetings, backlog maintenance, retrospectives and other principles of agile development.',
+      'Academic organizations',
+      'I am the vice-chairman of the Events Committee of the Student Union of the University of Helsinki. I participate in organizing events of my own subject organization TKO-äly ry. I will also be tutoring new students this fall.',
+      'Kaunialan hospital',
+      'I worked as an orderly in the summer of 2016 and 2017.'
+    ]
+  },
+  fi: {
+    bio: [
+      'Opiskelen tietojenkäsittelytiedettä toista vuotta Helsingin yliopistossa. Odotan innolla mahdollisuutta kokeilla kykyjeni rajoja. Tavoittelen käytännön kokemusta alalta hahmottaakseni tulevan urani suuntaa. Positiivisena, ulospäinsuuntautuneena opiskelijajärjestöaktiivina en pelkää poistua mukavuusalueeltani. Intohimoihini kuuluvat ohjelmointikielten lisäksi myös luonnolliset kielet. Kaksikielisyyteni helpottaa uusien kielten omaksumista. Haastan itseäni espanjan ja japanin opinnoissa.',
+      'Tekemäni Travel Tracker -sovellus',
+      '(React, Redux, Semantic.ui, Node.js, MongoDB)'
+    ],
+    skills: [
+      'Opinnoissani ja omissa projekteissani on käytössäni ollut Java, Python, HTML, CSS, Junit-testaus ja SQL-tietokannat.',
+      'Innostuin web-sovelluskehityksestä Full Stack -kurssilla. Tekemissäni sovelluksissa on käytössä React, Redux, Node.js ja MongoDB.',
+      'Tietojenkäsittelijän perustaidot ovat hyvässä hallussa. Komentotulkki Bash on päivittäisessä käytössä. Git-versionhallintaa olen käyttänyt kaikissa ohjelmistoprojekteissani. Isommat projektini käyttävät Herokua ja Travisia.'
+    ],
+    experience: [
+      'Ohjelmistoprojekti',
+      'Kehitimme ohjelmistotuotannon kurssilla kuuden hengen Scrum-tiimillä asiakkaan vaatimusten mukaisen ohjelmiston. Neljän sprintin aikana tutuiksi tulivat asiakastapaamiset, backlogin ylläpito, retrospektiivit ja muut ketterän kehittämisen perusteet.',
+      'Opiskelijajärjestöt',
+      'Olen Helsingin yliopiston ylioppilaskunnan tapahtumavaliokunnan varapuheenjohtaja. Osallistun oman ainejärjestöni TKO-äly ry:n tapahtumien järjestämiseen. Lisäksi tulen toimimaan uusien opiskelijoiden tuutorina tulevana syksynä.',
+      'Kaunialan sairaala',
+      'Työskentelin sairaala-apulaisena kesällä 2016 ja 2017.'
+    ]
+  }
+}
+
+module.exports = Data
---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const data = require('./Data')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+
+app.get('/data', (request, response) => {
+  response.json(data)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
