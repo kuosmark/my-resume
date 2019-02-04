df5b7511a3935abc0ad1ea516dc391ad3104b3a2
Markus Kuosmanen <markus.kuosmanen@helsinki.fi>
2019-02-05 01:21:25 +0200

    solved problem with routing

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const data = require('./Data')
 const express = require('express')
 const app = express()
@@ -7,6 +8,14 @@ app.use(cors())
 
 app.get('/data', (request, response) => {
   response.json(data)
+})
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, './build/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
 })
 
 const PORT = process.env.PORT || 3001
