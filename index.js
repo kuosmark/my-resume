0791bd41944a1058252ea94f7458da50d18afbea
Markus Kuosmanen <markus.kuosmanen@helsinki.fi>
2019-02-05 01:46:15 +0200

    another attempt

--- index.js
+++ index.js
@@ -6,23 +6,13 @@ const cors = require('cors')
 app.use(express.static('build'))
 app.use(cors())
 
-app
-  .get('/data', (req, res) => {
-    res.json(data)
-  })
-  .catch((error) => {
-    console.log(error)
-    res.status(500)
-  })
+app.get('/data', (req, res) => {
+  res.json(data)
+})
 
-app
-  .get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './build/index.html'))
-  })
-  .catch((error) => {
-    console.log(error)
-    res.status(500)
-  })
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, './build/index.html'))
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
